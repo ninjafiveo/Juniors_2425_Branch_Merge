0960518d0a07a6aeb33c6f43fd6762db710bcd61
Camanator563 <144284057+Camanator563@users.noreply.github.com>
2024-09-12 10:10:54 -0400

    finished task

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import sekol_michael
 import lastname_firstname
-
+import moore_cameron
 def introduce_yourself():
     sekol_michael.say_hello()
-    lastname_firstname.say_hello()
-
+    moore_cameron.say_hello()
 introduce_yourself()
 
 
