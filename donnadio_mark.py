1428b01d77dedd6acf5660a444ecc6d8873bf0f4
STORM <stormvtube@gmail.com>
2024-09-12 10:12:02 -0400

    Donnadio

---
+++ donnadio_mark.py
@@ -0,0 +1,4 @@
+my_name = "Mark Donnadio"
+def say_hello():
+    greeting = print(f"Hello, my name is {my_name}")
+    return greeting
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import sekol_michael
 import lastname_firstname
+import donnadio_mark
 
 def introduce_yourself():
     sekol_michael.say_hello()
     lastname_firstname.say_hello()
+    donnadio_mark.say_hello()
+    
 
 introduce_yourself()
 
