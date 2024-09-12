3d74bda8d639b5d1f78ab1e5bda9a8e529be5c79
Conner Farnsworth <Rediceangelus@gmail.com>
2024-09-12 10:06:27 -0400

    updates

---
+++ Farnsworth_Conner.py
@@ -0,0 +1,5 @@
+my_name = "Conner Farnsworth"
+def say_hello():
+    greeting = print(f"Hello, my name is {my_name}")
+    return greeting
+# say_hello()
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import sekol_michael
 import lastname_firstname
+import Farnsworth_Conner
 
 def introduce_yourself():
     sekol_michael.say_hello()
     lastname_firstname.say_hello()
+    Farnsworth_Conner.say_hello()
 
 introduce_yourself()
-
-
-
