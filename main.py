7254eae9618f2397182ab87f58b1d8d40c078ad6
ninjafiveo <mjsekol@live.com>
2024-09-12 08:20:30 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import sekol_michael
+
+def introduce_yourself():
+    sekol_michael.say_hello()
+
+introduce_yourself()
---
+++ sekol_michael.py
@@ -0,0 +1,6 @@
+my_name = "Mr. Sekol"
+def say_hello():
+    greeting = print(f"Hello, my name is {my_name}")
+    return greeting
+    
+# say_hello()
