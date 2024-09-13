45ea18d4c95b5e387dd61c89953b1b8e507495df
John Doe <johndoe@email.com>
2024-09-13 10:04:49 -0400

    added personal introduction in lastname_firstname.py, updated main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import sekol_michael
 import lastname_firstname
 import windsor_rigdon
+import diblasio_sonia
 
 
 def introduce_yourself():
     sekol_michael.say_hello()
     lastname_firstname.say_hello()
     windsor_rigdon.say_hello()
+    diblasio_sonia.say_hello()
 
 
 introduce_yourself()
