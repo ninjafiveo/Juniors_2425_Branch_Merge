1a4cbdf7737d5b14cc6e1b3b4ea62caf28dc07fd
nxtcarson <nxtcarson@gmail.com>
2024-09-13 09:06:25 -0400

    Added personalized introduction function in lastname_firstname.py and updated main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import sekol_michael
-import lastname_firstname
+import pugh_carson
 
 def introduce_yourself():
     sekol_michael.say_hello()
-    lastname_firstname.say_hello()
+    pugh_carson.say_hello()
 
 introduce_yourself()
 
---
+++ pugh_carson.py
@@ -0,0 +1,5 @@
+my_name = "Carson Pugh"
+def say_hello():
+    greeting = print(f"Hello, my name is {my_name}")
+    return greeting
+
