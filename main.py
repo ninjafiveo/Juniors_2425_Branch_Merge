a9c721aba122ed1e47313336795fea2cc2e05c72
DrRigB <rigdon.windsor@gmail.com>
2024-09-13 08:59:39 -0400

    added personalized introduction for my name. and updated main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import sekol_michael
 import lastname_firstname
+import windsor_rigdon
+
 
 def introduce_yourself():
     sekol_michael.say_hello()
     lastname_firstname.say_hello()
+    windsor_rigdon.say_hello()
+
 
 introduce_yourself()
 
--- windsor_rigdon.py
+++ windsor_rigdon.py
@@ -1,8 +1,4 @@
-my_name = "lastname_firstname"
+my_name = "Windsor Rigdon"
 def say_hello():
     greeting = print(f"Hello, my name is {my_name}")
     return greeting
-
-
-
-
