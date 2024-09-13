7cc33c6cfd64e5c9fc58ad2aa2edfb9662f15411
richi2008-jag <rbuchanan206@gmail.com>
2024-09-13 09:35:05 -0400

    Added personalized introduction function in lastname_firstname.py and updated main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import sekol_michael
 import lastname_firstname
+import buchanan_richi
 
 def introduce_yourself():
     sekol_michael.say_hello()
     lastname_firstname.say_hello()
-
+    buchanan_richi.say_hello()
 introduce_yourself()
 
 
