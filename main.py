3fb15dfe67ee2e5549246ed3a1327d3cea61e757
Brant-100 <Brantsimpson100@gmail.com>
2024-09-13 09:47:35 -0400

    Added personalized introduction function in lastname_firstname.py and updated main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import sekol_michael
-import lastname_firstname
+import simpson_brant
 
 def introduce_yourself():
     sekol_michael.say_hello()
-    lastname_firstname.say_hello()
+    simpson_brant.say_hello()
 
 introduce_yourself()
 
---
+++ simpson_brant.py
@@ -0,0 +1,6 @@
+my_name = "Simpson Brant"
+def say_hello():
+    greeting = print(f"Hello, my name is {my_name}")
+    return greeting
+
+#say_hello()
