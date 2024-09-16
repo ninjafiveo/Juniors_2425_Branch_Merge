e861ede6a8b2d8eb1f6f400d94696482457ac199
NateLawrence-w <Natewolf676@gmail.com>
2024-09-16 08:04:48 -0400

    Added the Wiseman Files

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import sekol_michael
 import lastname_firstname
 import windsor_rigdon
-
+import Wiseman_Nathan
 
 def introduce_yourself():
     sekol_michael.say_hello()
     lastname_firstname.say_hello()
     windsor_rigdon.say_hello()
+    Wiseman_Nathan.say_hello()
 
 
 introduce_yourself()
